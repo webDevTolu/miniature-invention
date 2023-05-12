719b2f908c0ad1e5be5891ef5cff0f7de825892f
tolu-c <tolu.adeyemo7@gmail.com>
2023-05-12 15:40:57 +0100

    error code

--- components/ui/footer/newsletter.tsx
+++ components/ui/footer/newsletter.tsx
@@ -2,7 +2,7 @@ import {  FormEvent, useState } from "react";
 import { Button } from "../form/Button";
 import { Input } from "../form/Input";
 
-const Newsletter = () => {
+const Newsletter = (props) => {
   const [enteredEmail, setEnteredEmail] = useState<string>('')
 
   const handleEmailChange = (email: string) => {
